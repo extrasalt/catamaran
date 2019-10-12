027c58cab7c99f8d0678f7e83b90ccd92ed3010c
Mohanarangan Muthukumar <mohan@extrasalt.org>
2019-10-12 15:08:58 +0530

    Remove material-ui

--- src/App.js
+++ src/App.js
@@ -2,42 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 
 import "./App.css";
-
-function DetailedView() {
-  return (
-    <div>
-      <div className="card">
-        <h3>Ticket Info</h3>
-        <div>
-          <span> Status: </span> <span> Dispatched </span>
-        </div>
-        <div>
-          <span> Type: </span> <span> Supplies </span>
-        </div>
-        <div>
-          <span> Message: </span> <span> Fire!! Fire!! </span>
-        </div>
-        <div>
-          <span> Address: </span> <span> OMR perungudi </span>
-        </div>
-        <div>
-          <span> Number: </span> <span> 1234</span>
-        </div>
-      </div>
-      <VolunteerCard />
-    </div>
-  );
-}
-
-function VolunteerCard() {
-  return (
-    <div class="card">
-      <h3>Volunteer Details</h3>
-      Volunteer name <br />
-      82918391823
-    </div>
-  );
-}
+import DetailedView from "./DetailedView.js";
 
 function Nav() {
   var style = {
---
+++ src/DetailedView.js
@@ -0,0 +1,50 @@
+import React from "react";
+import logo from "./logo.svg";
+
+import "./App.css";
+
+function DetailedView() {
+  return (
+    <div>
+      <div className="card">
+        <h3>Ticket Info</h3>
+        <div>
+          <span> Status: </span> <span> Dispatched</span>
+        </div>
+        <div>
+          <span> Type: </span> <span> Supplies </span>
+        </div>
+        <div>
+          <span> Message: </span> <span> Fire!! Fire!! </span>
+        </div>
+        <div>
+          <span> Address: </span> <span> OMR perungudi </span>
+          <button>Map</button>
+        </div>
+
+        <div>
+          <span> Number: </span> <span> 1234</span>
+        </div>
+      </div>
+      <ShareOnWhatsapp message="Fire!! Fire!!" />
+      <VolunteerCard />
+    </div>
+  );
+}
+
+function ShareOnWhatsapp(props) {
+  var whatsappLink = "whatsapp://send?text=" + props.message;
+  return <a href={whatsappLink}> Share on whatsapp </a>;
+}
+
+function VolunteerCard() {
+  return (
+    <div class="card">
+      <h3>Volunteer Details</h3>
+      Volunteer name <br />
+      82918391823
+    </div>
+  );
+}
+
+export default DetailedView;
