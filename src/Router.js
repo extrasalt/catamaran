5391d978774f96ef8093a38e1a5f6d241ad2cf12
Mohanarangan Muthukumar <mohan@extrasalt.org>
2019-10-12 16:36:54 +0530

    Add braces

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import IssueForm from './forms/IssueForm';
-import DetailedView from './DetailedView';
+import React from "react";
+import IssueForm from "./forms/IssueForm";
+import { DetailedView } from "./DetailedView";
 
 const routes = {
-    "/create": () => <IssueForm />,
-    "/show": () => <DetailedView />,
-  };
-  export default routes;
+  "/create": () => <IssueForm />,
+  "/show": () => <DetailedView />
+};
+export default routes;
