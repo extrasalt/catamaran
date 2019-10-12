8a3693797018e9d16bd91e2fedc2d3562fcf7244
Mohanarangan Muthukumar <mohan@extrasalt.org>
2019-10-12 14:44:04 +0530

    Add basic issue detail

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function DetailedView() {
+  return (
+    <div>
+      <div className="card">
+        <h3>Ticket Info</h3>
+        <div>
+          <span style={{}}> Status: </span> <span> Dispatched </span>
+        </div>
+        <div>
+          <span> Type: </span> <span> Supplies </span>
+        </div>
+        <div>
+          <span> Message: </span> <span> Fire!! Fire!! </span>
+        </div>
+        <div>
+          <span> Address: </span> <span> OMR perungudi </span>
+        </div>
+        <div>
+          <span> Number: </span> <span> 1234</span>
+        </div>
+      </div>
+      <VolunteerCard />
+    </div>
+  );
+}
+
+function VolunteerCard() {
+  return (
+    <div class="card">
+      <h3>Volunteer Details</h3>
+      Volunteer name <br />
+      82918391823
+    </div>
+  );
+}
+
+function Nav() {
+  var style = {
+    backgroundColor: "red",
+    width: "100%",
+    height: 40
+  };
+
+  return <div style={style}> </div>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <DetailedView />
     </div>
   );
 }
