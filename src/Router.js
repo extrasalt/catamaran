1d9b1650921b265026557b1ef67f99387011e7de
Prasanna Jagathesan <prasanna.jagathesan@avalara.com>
2019-10-12 18:38:03 +0530

    Adding sign in page

--- src/Router.js
+++ src/Router.js
@@ -3,11 +3,13 @@ import IssueForm from './forms/IssueForm';
 import { DetailedView } from './DetailedView';
 import RegistrationForm from './forms/Registration';
 import BoardView from "./BoardView";
+import SigninForm from './forms/Signin';
 
 const routes = {
     "/create": () => <IssueForm />,
     "/show/:id": ({id}) => <DetailedView id={id} />,
     "/register": () => <RegistrationForm />,
+    "/signin": () => <SigninForm />,
     "/": () => <BoardView />
   };
 export default routes;
